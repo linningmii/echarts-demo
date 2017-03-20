fde0fa1af4f83b1ffd5f4c421f3f23637a10c48d
林中鸣 <linzhongming@linzhongmingdeMac-mini.local>
2017-03-20 19:23:55 +0800

    First chart

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -7,10 +8,22 @@ module.exports = {
         filename: 'bundle.js?[hash]',
         path: path.resolve(__dirname, './dist')
     },
-    module: {},
+    module: {
+        rules: [{
+            test: /\.pug$/,
+            use: 'pug-loader'
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'sass-loader']
+            })
+        }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html')
-        })
+            template: path.resolve(__dirname, './src/index.pug'),
+            favicon: path.resolve(__dirname, './public/favicon.png')
+        }),
+        new ExtractTextPlugin('style.css?[hash]')
     ]
 };
