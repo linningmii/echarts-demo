4d72bdcb9ee1fbab2f3ed99f0585b032e128e4ac
林中鸣 <linzhongming@linzhongmingdeMac-mini.local>
2017-03-20 19:58:56 +0800

    bar chart and pie chart

--- src/index.js
+++ src/index.js
@@ -2,18 +2,40 @@ import './style.scss';
 
 const eCharts = require('echarts');
 
-const myChart = eCharts.init(document.getElementById('main'));
+const barChart = eCharts.init(document.getElementById('bar-chart'));
 
-myChart.setOption({
-    title: { text: 'ECharts 入门示例' },
-    tooltip: {},
-    xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-    },
-    yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
+barChart.setOption({
+  title: {
+    text: '柱形图'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20]
+  }]
+});
+
+const pieChart = eCharts.init(document.getElementById('pie-chart'));
+
+pieChart.setOption({
+  title: {
+    text: '饼图'
+  },
+  series: [{
+    name: '访问来源',
+    type: 'pie',
+    radius: '80%',
+    data: [
+      {value: 400, name: '搜索引擎'},
+      {value: 335, name: '直接访问'},
+      {value: 310, name: '邮件营销'},
+      {value: 274, name: '联盟广告'},
+      {value: 235, name: '视频广告'}
+    ]
+  }]
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin');
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-        filename: 'bundle.js?[hash]',
-        path: path.resolve(__dirname, './dist')
-    },
-    module: {
-        rules: [{
-            test: /\.pug$/,
-            use: 'pug-loader'
-        }, {
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-                use: ['css-loader', 'sass-loader']
-            })
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.pug'),
-            favicon: path.resolve(__dirname, './public/favicon.png')
-        }),
-        new ExtractTextPlugin('style.css?[hash]')
-    ]
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    filename: 'bundle.js?[hash]',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      use: 'pug-loader'
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        use: ['css-loader', 'sass-loader']
+      })
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.pug'),
+      favicon: path.resolve(__dirname, './public/favicon.png')
+    }),
+    new ExtractTextPlugin('style.css?[hash]')
+  ]
 };
